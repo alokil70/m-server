90ff772542a47308b961b9d928701ac92da9437e
Alexandr <Alokil70@gmail.com>
2021-09-23 21:06:33 +0300

    q

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,18 +1,1 @@
-import { ConnectionOptions } from 'typeorm';
-
-const config: ConnectionOptions = {
-	type: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	username: 'postgres',
-	password: '1234',
-	database: 'mediumclone',
-	entities: [__dirname + '/**/*.entity{.ts,.js}'],
-	synchronize: false,
-	migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-	cli: {
-		migrationsDir: 'src/migrations',
-	},
-};
-
-export default config;
+import { ConnectionOptions } from 'typeorm';const config: ConnectionOptions = {	type: 'postgres',	host: 'localhost',	port: 5432,	username: 'postgres',	password: '1234',	database: 'mediumclone',	entities: [__dirname + '/**/*.entity{.ts,.js}'],	synchronize: false,	migrations: [__dirname + '/migrations/**/*{.ts,.js}'],	cli: {		migrationsDir: 'src/migrations',	},};export default config;
